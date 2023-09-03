c1b4c8eb4819b9e1dd86d1b697dc87d7b2240214
amr mahmoud <amrmahmoud1900@gmail.com>
2023-09-03 17:53:14 +0300

    first commit

---
+++ src/main/java/com/example/nabd/NabdApplication.java
@@ -0,0 +1,13 @@
+package com.example.nabd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NabdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NabdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nabd/NabdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nabd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
