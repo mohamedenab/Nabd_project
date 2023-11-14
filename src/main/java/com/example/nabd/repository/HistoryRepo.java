dd4073a50a7f3c8f81c661b58093ae099c810b5a
amr mahmoud <amrmahmoud1900@gmail.com>
2023-11-14 11:52:25 +0200

    update HistoryRepo

--- src/main/java/com/example/nabd/repository/HistoryRepo.java
+++ src/main/java/com/example/nabd/repository/HistoryRepo.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Repository
 public interface HistoryRepo extends JpaRepository<History,Long> {
-    History findByLocationPatientH(Patient patient);
+//    History findByLocationPatientH(Patient patient);
 }
