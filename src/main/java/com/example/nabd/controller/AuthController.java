74075c7957c2ca5c2405ba9dcea560a3ee36d717
amr mahmoud <amrmahmoud1900@gmail.com>
2025-02-15 11:09:46 +0200

    test

--- src/main/java/com/example/nabd/controller/AuthController.java
+++ src/main/java/com/example/nabd/controller/AuthController.java
@@ -42,7 +42,7 @@ public class AuthController {
     }
     @GetMapping("/login/1")
     public ResponseEntity<String> loginFun2(){
-        return ResponseEntity.ok("tmam22");
+        return ResponseEntity.ok("tmam222");
     }
     @Operation(
             summary = "Add new User",
